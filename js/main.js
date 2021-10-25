e01f1c3a4e2286755cf4e8aff6f5ff004cccae27
“Cal-107” <kaly___7@hotmail.it>
2021-10-25 17:42:58 +0200

    add final part

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log('js ok');
+let boxes = document.querySelector('.boxes');
+
+for (let i = 1; i <= 100; i++) {
+    let className = '';
+    let text = '';
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        className="fizz-buzz-box"
+        text="fizzbuzz"
+    } 
+
+    else if (i % 3 == 0) {
+        className="fizz-box"
+        text= "fizz"
+    }
+
+    else if (i % 5 == 0) {
+        className="buzz-box"
+        text="buzz"
+    }
+    
+    else {
+       className="default";
+       text = i;
+    }
+
+    boxes.innerHTML += `<span class="box ${className}">${text}</span>`;
+}
